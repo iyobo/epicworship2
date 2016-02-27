c7aa026d91f3357bc2cb74a1656ab1ea804fb758
iyobo Eki <phil.eki@gmail.com>
2016-02-27 10:26:46 -0800

    Projector

--- init.js
+++ init.js
@@ -4,12 +4,19 @@
  * @see http://developer.chrome.com/apps/app.runtime.html
  * @see http://developer.chrome.com/apps/app.window.html
  */
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create(
-    'index.html',
-    {
-      id: 'mainWindow',
-      bounds: {width: 800, height: 600}
-    }
-  );
+chrome.app.runtime.onLaunched.addListener(function (launchData) {
+    chrome.app.window.create(
+        'screens/dashboard.html',
+        {
+            id: 'dashboardWindow',
+            bounds: {width: 800, height: 600}
+        }
+    );
+    chrome.app.window.create(
+        'screens/projector.html',
+        {
+            id: 'projectorWindow',
+            bounds: {width: 900, height: 800}
+        }
+    );
 });
