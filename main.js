a9464c17ffb02fdc2aaf1c78597e4a358c596838
iyobo Eki <phil.eki@gmail.com>
2016-02-27 06:13:36 -0800

    changes

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
+function transition(){}
+
 window.onload = function() {
   var vid = $("#bgvid");
   var whiteout = $("#whiteout_layer");
   
   whiteout.addClass('animated fadeOut');
   
-  // whiteout.fadeOut(800);
-  // document.querySelector('#greeting').innerText = 'Hello, World! It is. Blessed be your name when I walk ithru theon fosinso snils sfs slisnl gjslngsi gsn dfjsindfs j' + new Date();
   $("#bgbtn").on("click",function(e){
     $('#bgvid source').attr('src', "assets/bright.mp4");
-    
-    whiteout.addClass('animated fadeOut');
-    whiteout.fadeIn(500,function(){
+    whiteout.removeClass('fadeOut');
+    whiteout.addClass('fadeIn');
+    whiteout.one('animationend', function(){
       vid[0].load();
-      whiteout.fadeOut(800,function(){
-        
-      })
+      whiteout.removeClass('fadeIn');
+      whiteout.addClass('fadeOut');
     });
-    
-    //Transiti
-    // vid.fadeOut(500,"linear",function(){
-    //   vid[0].load();
-    //   vid.fadeIn(800,"linear");  
-      
-    // });
-    
     
   })
 };
