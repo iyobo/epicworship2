9c9313395911334e4f2954445092d9de7d16ebb0
iyobo Eki <phil.eki@gmail.com>
2016-02-27 08:36:42 -0800

    setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,10 @@ function transition(){}
 
 window.onload = function() {
   var vid = $("#bgvid");
-  // var whiteout = $("#whiteout_layer");
-  // whiteout.addClass('animated fadeOut');
   
   $("#bgbtn").on("click",function(e){
     $('#bgvid source').attr('src', "bg/video/bright.mp4");
-    // whiteout.removeClass('fadeOut');
+
     vid.addClass('animated fadeOut');
     vid.one('animationend', function(){
       vid[0].load();
